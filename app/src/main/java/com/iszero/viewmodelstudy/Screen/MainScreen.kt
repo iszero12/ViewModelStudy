27bfab5d18fa2548449a39204947fe565a2562bf
iszero12 <binj1246@gmail.com>
2024-09-10 23:05:27 +0900

    feat::버튼 누르면 바로 반영되게 함

--- app/src/main/java/com/iszero/viewmodelstudy/Screen/MainScreen.kt
+++ app/src/main/java/com/iszero/viewmodelstudy/Screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.iszero.viewmodelstudy.Screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,28 +8,28 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.iszero.viewmodelstudy.ViewModel.MainViewModel
 
 @Composable
-fun MainScreen(viewModel: MainViewModel,modifier: Modifier) {
-    val data:String = viewModel.data.value.toString()
+fun MainScreen(viewModel: MainViewModel, modifier: Modifier) {
+    val data = viewModel.data.observeAsState(initial = "")
 
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
     ) {
-        var text by remember { mutableStateOf("") }
-        TextField(value = text, onValueChange = { text = it })
-        Button(onClick = { viewModel.updateData(text)}) {
+        var (text, setValue) = remember { mutableStateOf("") }
+        TextField(value = text, onValueChange = setValue)
+        Button(onClick = { viewModel.updateData(text) }) {
+            Text(text = "Update")
+            Log.d("MainScreen", "MainScreen: ${data.value}")
         }
-        Text(text = if (data == null) "No Data" else data)
+        Text(text = data.value ?: "")
     }
 }
